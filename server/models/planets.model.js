fa4178e61ce9be931adbeed2d7829ea5291a39c0
Alexandru <thealexandru2292@gmail.com>
2024-03-29 22:15:18 +0200

    using the code from index.js of planets-project and the kepler_data.csv in the planets.model

--- server/models/planets.model.js
+++ server/models/planets.model.js
@@ -1,3 +1,45 @@
-const planets = [];
+const { parse } = require('csv-parse');
+const fs = require('fs');
 
-module.exports = planets; 
+const habitablePlanets = [];
+
+
+function isHabitablePlanet(planet)
+{
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+}
+
+fs.createReadStream('./data/kepler_data.csv')
+//connect parse() with fs together by pipe()
+//it transform data from stream (0101) to bytes data (text)
+    .pipe(parse(
+        {
+            //dependign on who wrote the csv file put the comment sign and we need to indicate which character is the commented line
+            comment: '#',
+            columns: true,//set columns as key=>value pairs
+        }
+    ))
+    .on('data', (data) => {
+
+        if(isHabitablePlanet(data))
+        {
+            habitablePlanets.push(data);
+        }
+        
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', () => {
+        console.log(habitablePlanets.map((planet) => {
+            return planet['kepler_name'];
+        }));
+        console.log(`${habitablePlanets.length} habitable planets found`);
+        console.log('done');
+    })
+
+module.exports = {
+    planets: habitablePlanets
+}; 
