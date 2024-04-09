46037ac9b9c27342e8830386313b6b3ebc9ed831
Alexandru <thealexandru2292@gmail.com>
2024-04-09 16:03:30 +0300

    Testing APIs added supertest library and tested GET Request

--- server/routes/launches/launches.test.js
+++ server/routes/launches/launches.test.js
@@ -1,10 +1,15 @@
-// we can create test files like this or like launches.spec.js or creating a folder __tests__ in serrver folder
-// this will include tests for our launches endpoints
-//the describe() function is not a JS or Node function it is Jest function that runs whenm we type: "npm test" from server folder
+//installed npm supertest --save-dev to perform call test agaings api 
+//we need to rquire the tool that makes request against api
+const request = require('supertest');
+//because request(); requires app as parameter we we will require our app.js 
+const app = require('../../src/app');
+
 describe('Test GET /launches', () => {
-    test('It Should respond with 200 success', () => {
-        const response = 200;
-        expect(response).toBe(200);
+    test('It Should respond with 200 success', async () => {
+        const response = await request(app)
+        .get('/launches')
+        .expect('Content-Type', /json/)
+        .expect(200);
     })
 });
 
