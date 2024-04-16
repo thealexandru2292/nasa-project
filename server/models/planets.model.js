09c419295b016b6d722f49f7eb5194adb1c5a652
Alexandru <thealexandru2292@gmail.com>
2024-04-16 14:54:08 +0300

    How Find() operators work

--- server/models/planets.model.js
+++ server/models/planets.model.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const { parse } = require('csv-parse');
 
+const planets = require('./planets.mongo');
+
 const habitablePlanets = [];
 
 
@@ -12,37 +14,24 @@ function isHabitablePlanet(planet)
         && planet['koi_prad'] < 1.6;
 }
 
-/* 
-const promise = new Promise((resolve, reject) => {
-    resolve(42); 
-});
-//42 is passes in (result) => {}
-promise.then((result) => {
-
-})
-
-const result = await promise;
-*/
-
-//we return a promise which resolves which habital planets have been found
-//and we will wait for that promise to resolve before listening to requests in our server
 function loadPlanetsData(){
     return new Promise((resolve, reject) => {
         fs.createReadStream(path.join(__dirname,'..','..','server','data','kepler_data.csv'))
-            //connect parse() with fs together by pipe()
-            //it transform data from stream (0101) to bytes data (text)
         .pipe(parse(
             {
-                //dependign on who wrote the csv file put the comment sign and we need to indicate which character is the commented line
+ 
                 comment: '#',
-                columns: true,//set columns as key=>value pairs
+                columns: true,
             }
         ))
-        .on('data', (data) => {
+        .on('data', async (data) => {
     
             if(isHabitablePlanet(data))
             {
-                habitablePlanets.push(data);
+            /*  TODO: replace create with upsert
+                await planets.create({
+                    keplerName: data.kepler_name
+                }); *///this will be saved as planet object on a new document that will be stored in planets collection in mongoDB
             }
             
         })
@@ -59,8 +48,30 @@ function loadPlanetsData(){
     });
 }
 
-function getAllPlanets(){
-    return habitablePlanets;
+async function getAllPlanets(){
+    return await planets.find({
+       //if we pass an empty object all documents will be returned
+       //if we pass keplerName: 'Kepler-62 f', only planets matchign this name will be returned
+    }//,
+    //{
+        /* our find() method takes an second argument also an object which is an projection, 
+           the list of fields that you would like to include in the list
+        */
+      // 'keplerName': 1, //this will show the kepler name field, if 0 mongo will exclude this 
+                        //or we can just use instead of this object, a list of fields separated by space as a string 'keplerName antotherField'
+                        //and if we would like to exlcude keplerName and only add anotherField we add - simbol in front of the field we want to exclude
+                        // '-keplerName antotherField' 
+
+                        // find more about find() here : https://mongoosejs.com/docs/api/model.html#Model.find()
+                       // User.find({ age: { $gte: 21, $lte: 65 } });
+                       //$gte - greater than or equal to
+                       //$lte - less than or equal to
+                       
+                       // executes, name LIKE john and only selecting the "name" and "friends" fields
+                       //await MyModel.find({ name: /john/i }, 'name friends').exec();
+
+    //}
+    );
 }
 
 module.exports = {
--- server/routes/planets/planets.controller.js
+++ server/routes/planets/planets.controller.js
@@ -1,8 +1,8 @@
 const {getAllPlanets} = require('../../models/planets.model');
 
-function httpGetAllPlanets(req, res)
+async function httpGetAllPlanets(req, res)
 {
-   return res.status(200).json(getAllPlanets()); 
+   return res.status(200).json(await getAllPlanets()); 
    //with return we make sure the response is provided only once, and headers are also being already set
 }
 
